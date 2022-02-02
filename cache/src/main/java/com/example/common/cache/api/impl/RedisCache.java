e9cacd7d30644a4ffc83719ed33423c888d1369d
xiapeng <xiapeng@didiglobal.com>
2022-02-02 23:07:11 +0800

    RedisCache

--- cache/src/main/java/com/example/common/cache/api/impl/RedisCache.java
+++ cache/src/main/java/com/example/common/cache/api/impl/RedisCache.java
@@ -1,14 +1,18 @@
 package com.example.common.cache.api.impl;
 
 import com.example.common.cache.api.CacheApi;
+import com.example.common.cache.constants.Constant;
 import com.example.common.util.BeanUtil;
+import com.example.common.util.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
 
 /**
  * @Author xiapeng
  * @Date: 2022/01/31/12:08 上午
- * @Description:
+ * @Description:Redis缓存
  */
+@Slf4j
 public enum RedisCache implements CacheApi {
     /**
      * 实例
@@ -25,16 +29,45 @@ public enum RedisCache implements CacheApi {
 
     @Override
     public <T> T get(String key, Class<T> clazz) {
+        String operation = "get";
+        try {
+            String value = jedis.get(key);
+            log.debug(Constant.SUCCESS, NAME, operation, key, value);
+        } catch (Exception e) {
+            log.error(Constant.FAIL, NAME, operation, key, "", e);
+        }
         return null;
     }
 
     @Override
     public void set(String key, Object value, int expire) {
-
+        String operation = "set";
+        try {
+            String val = value == null ? "" : JsonUtil.toString(value);
+            String result = jedis.set(key, val);
+            if (OK.equals(result)) {
+                log.debug(Constant.SUCCESS, NAME, operation, key, value);
+            } else {
+                log.debug(Constant.FAIL, NAME, operation, key);
+            }
+            jedis.expire(key, expire);
+        } catch (Exception e) {
+            log.error(Constant.FAIL, NAME, operation, key, "", e);
+        }
     }
 
     @Override
     public void remove(String key) {
-
+        String operation = "remove";
+        try {
+            Long result = jedis.del(key);
+            if (result > 0) {
+                log.debug(Constant.SUCCESS, NAME, operation, key);
+            } else {
+                log.debug(Constant.FAIL, NAME, operation, key);
+            }
+        } catch (Exception e) {
+            log.error(Constant.FAIL, NAME, operation, key, "", e);
+        }
     }
 }
