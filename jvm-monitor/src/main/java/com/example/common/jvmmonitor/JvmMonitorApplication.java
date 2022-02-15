ae2c8b3ce1128b8865cc1785547523e8bddc5483
夏先鹏 <951256916>
2022-02-15 21:48:35 +0800

    jvm-monitor

---
+++ jvm-monitor/src/main/java/com/example/common/jvmmonitor/JvmMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.common.jvmmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmMonitorApplication.class, args);
+    }
+
+}
---
+++ jvm-monitor/src/test/java/com/example/common/jvmmonitor/JvmMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.common.jvmmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
